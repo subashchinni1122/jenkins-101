bc2df66c2fddc862b2f4239ca64a50a64c4b5592
subashchinni1122 <subashchinni1122@gmail.com>
2026-04-02 00:37:29 +0530

    Fix syntax for pipeline definition in Groovy script

--- vars/runUFTWebPipeline.groovy
+++ vars/runUFTWebPipeline.groovy
@@ -1,13 +1,14 @@
 def call()
 {
-    pipeline{
+    pipeline
+    {
         agent{label 'EC2Instance'}
         stages{
             stage(Install Chrome')
                   {
                       steps{installChrome()}
                   }
-        }
+            }
     }
     #sh 'echo Hi this is from Devops and its a Shared Library'
 }
