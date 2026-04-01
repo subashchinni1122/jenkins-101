161ce3d271835e94d3b879366f99b8ba5fed749f
subashchinni1122 <subashchinni1122@gmail.com>
2026-04-02 00:36:56 +0530

    Convert script to pipeline for Chrome installation
    Refactor to use pipeline structure for installing Chrome.

--- vars/runUFTWebPipeline.groovy
+++ vars/runUFTWebPipeline.groovy
@@ -1,6 +1,13 @@
 def call()
 {
-    def scriptContent=libraryResource('scripts/install_chrome.sh')
-    sh 'chmod +x ./install_chrome.sh'
+    pipeline{
+        agent{label 'EC2Instance'}
+        stages{
+            stage(Install Chrome')
+                  {
+                      steps{installChrome()}
+                  }
+        }
+    }
     #sh 'echo Hi this is from Devops and its a Shared Library'
 }
